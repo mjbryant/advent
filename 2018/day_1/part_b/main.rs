847bdc35e2ca54fbc6ee6b345991be28a1301e40
Michael Bryant <mbryantj@gmail.com>
2019-09-18 13:21:42 -0600

    Day 1, part B complete

---
+++ 2018/day_1/part_b/main.rs
@@ -0,0 +1,35 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+/**
+ * Goal: starting at 0, sum numbers from the file (cycling over from the
+ * beginning if needed) until a cumulative sum you've already seen repeats.
+ */
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = match args.len() {
+        1 => "input.txt",
+        _ => &args[1],
+    };
+
+    let reader = BufReader::new(File::open(filename).expect("Failed to read file"));
+    let numbers: Vec<i32> = reader.lines().map(|line_result| {
+        let line = line_result.unwrap();
+        line.parse::<i32>().unwrap()
+    }).collect();
+
+    let mut seen = HashSet::new();
+    let endless_numbers = numbers.iter().cycle();
+    let mut total = 0;
+
+    for number in endless_numbers {
+        total += number;
+        if seen.contains(&total) {
+            println!("First repeated total is {:?}", total);
+            return
+        }
+        seen.insert(total);
+    }
+}
