b975e2f8b718516afcb6e1020dd3b98e3bdfc1d6
Michael Bryant <mbryantj@gmail.com>
2019-09-18 15:00:20 -0600

    Day 2, part b

--- 2018/day_2/part_b/main.rs
+++ 2018/day_2/part_b/main.rs
@@ -2,6 +2,22 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn get_shared_chars(a: &str, b: &str) -> String {
+    let mut chars_b = b.chars();
+    let mut shared = String::new();
+    for a_char in a.chars() {
+        let b_char = chars_b.next().unwrap();
+        if a_char == b_char {
+            shared.push(a_char);
+        }
+    }
+    return shared;
+}
+
+/**
+ * Goal: find the two IDs from the file that differ by exactly one character in
+ * the same position. We can just write an O(n^2) algorithm to do this easily.
+ */
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = match args.len() {
@@ -9,4 +25,15 @@ fn main() {
         _ => &args[1],
     };
     let reader = BufReader::new(File::open(filename).expect("Failed to read file"));
+    let lines: Vec<String> = reader.lines().map(|line_result| line_result.unwrap()).collect();
+
+    for i in 0..lines.len() {
+        for j in i..lines.len() {
+            let shared_chars = get_shared_chars(&lines[i], &lines[j]);
+            if shared_chars.len() == (lines[i].len() - 1) {
+                println!("Overlapping chars: {:?}", shared_chars);
+                return
+            }
+        }
+    }
 }
