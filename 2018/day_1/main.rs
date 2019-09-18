451cf7168489cd9da9aee44abb7ba9dc35450921
Michael Bryant <mbryantj@gmail.com>
2019-09-18 11:10:39 -0600

    Day 1 complete

---
+++ 2018/day_1/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = match args.len() {
+        1 => "input.txt",
+        _ => &args[1],
+    };
+    let reader = BufReader::new(File::open(filename).expect("Failed to read file"));
+    let mut total = 0;
+    for line_result in reader.lines() {
+        // Need to do this in a separate line from as_str to make line live longer
+        let line = line_result.unwrap();
+        // Ignore empty lines
+        if line.len() > 0 {
+            let mut chars = line.chars();
+            match chars.next().unwrap() {
+                '-' => {
+                    // Collect the rest of the characters into a string again
+                    let amount_str = chars.collect::<String>();
+                    let amount = amount_str.parse::<i32>().unwrap();
+                    total -= amount;
+                },
+                '+' => {
+                    let amount_str = chars.collect::<String>();
+                    let amount = amount_str.parse::<i32>().unwrap();
+                    total += amount;
+                },
+                _ => {}
+            }
+        }
+    }
+    println!("Final sum: {:?}", total);
+}
