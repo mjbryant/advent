bc33c7097e9ce5cd72f667d40423e1421abc91fc
Michael Bryant <mbryantj@gmail.com>
2019-09-19 15:50:44 -0600

    Day 5, part a

---
+++ 2018/day_5/part_a/main.rs
@@ -0,0 +1,51 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+fn should_react(a: &char, b: &char) -> bool {
+    (a.is_ascii_uppercase() ^ b.is_ascii_uppercase()) &&
+        (a.to_ascii_lowercase() == b.to_ascii_lowercase())
+}
+
+fn react_polymer(b: &mut Vec<u8>) {
+    let mut i: usize = 1;
+    while i < b.len() as usize {
+        if i == 0 {
+            i += 1;
+        }
+        if should_react(&(b[i] as char), &(b[i-1] as char)) {
+            b.remove(i-1);
+            b.remove(i-1);
+            i -= 1;
+        } else {
+            i += 1;
+        }
+    }
+}
+
+/**
+ * Goal: given a string of "reagents", two adjacent reagents react and cancel
+ * each other out if they're the same character but a different case. React the
+ * entire string until there are no more reactions.
+ *
+ * Approach: keep a pointer to a reagent. Try to react it with the reagent
+ * before it in the string. If it works, remove both of those and decrement the
+ * pointer. If not, advance the pointer.
+ */
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = match args.len() {
+        1 => "input.txt",
+        _ => &args[1],
+    };
+    let mut file = File::open(filename).expect("Failed to read file");
+    let mut s = String::new();
+    file.read_to_string(&mut s).expect("Failed to read file into String");
+    s = String::from(s.trim());
+    // Since the entire string is ASCII this should be fine, as each char is
+    // going to fit in one byte.
+    let mut b = s.into_bytes();
+    println!("Started with a polymer of length {:?}", b.len());
+    react_polymer(&mut b);
+    println!("Ended with a polymer of length {:?}", b.len());
+}
