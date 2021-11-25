6e2f3adaf00009d8f0a219159b5db28144a52b49
Michael Bryant <mbryantj@gmail.com>
2021-11-25 10:58:13 -0700

    [correct] 2019.2.a in Ruby

--- 2019/day1.rb
+++ 2019/day1.rb
@@ -1,17 +1,19 @@
+require './util'
+
 def compute(x)
   (x / 3) - 2
 end
 
-def part_1(filename)
+def part_a(filename)
   # For each number, divide by three, round down, then subtract two. 
   # Sum them all together
   result = File.open(filename).readlines.map do |line|
     compute(line.to_i)
   end.sum
-  puts "Part 1 result is #{result}"
+  print_result(result: result, day: 1, part: "a")
 end
 
-def part_2(filename)
+def part_b(filename)
   result = File.open(filename).readlines.map do |line|
     current = compute(line.to_i)
     total = 0
@@ -21,8 +23,8 @@ def part_2(filename)
     end
     total
   end.sum
-  puts "Part 2 result is #{result}"
+  print_result(result: result, day: 1, part: "b")
 end
 
-part_1("day1.txt")
-part_2("day1.txt")
+part_a("./data/day1.txt")
+part_b("./data/day1.txt")
---
+++ 2019/day2.rb
@@ -0,0 +1,43 @@
+require './util'
+
+def part_a(filename)
+  opcodes = File.open(filename).read.split(",").map(&:to_i)
+  opcodes[1] = 12
+  opcodes[2] = 2
+  new_opcodes = compute(opcodes)
+  print_result(result: new_opcodes[0], day: 2, part: "a")
+end
+
+def part_b(filename)
+end
+
+def compute(opcodes)
+  new_opcodes = opcodes.dup
+  position = 0
+  while new_opcodes[position] != 99
+    a_index = new_opcodes[position + 1]
+    b_index = new_opcodes[position + 2]
+    c_index = new_opcodes[position + 3]
+    if new_opcodes[position] == 1
+      new_opcodes[c_index] = new_opcodes[a_index] + new_opcodes[b_index]
+    elsif new_opcodes[position] == 2
+      new_opcodes[c_index] = new_opcodes[a_index] * new_opcodes[b_index]
+    else
+      raise StandardError.new("Incorrect opcode at position #{position}: #{new_opcodes[position]}")
+    end
+    position += 4
+  end
+  return new_opcodes
+end
+
+def test
+  assert_equal(compute([1,0,0,0,99]), [2,0,0,0,99])
+  assert_equal(compute([2,3,0,3,99]), [2,3,0,6,99])
+  assert_equal(compute([2,4,4,5,99,0]), [2,4,4,5,99,9801])
+  assert_equal(compute([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99])
+  puts "All tests pass"
+end
+
+part_a("./data/day2.txt")
+part_b("./data/day2.txt")
+test
