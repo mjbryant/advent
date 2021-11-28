365f42a4899aecb054495ae7b84ce7ea985e5651
Michael Bryant <mbryantj@gmail.com>
2021-11-28 09:27:44 -0700

    [correct] 2019.4.b in Ruby

--- 2019/day4.rb
+++ 2019/day4.rb
@@ -20,6 +20,20 @@ def part_a
   print_result(result: result, day: 4, part: "a")
 end
 
+def part_b
+  # Same as part a, except that the two adjacent digits can't be
+  # part of a larger group of matching digits. It can have other
+  # larger groups of matching digits, but must have at least one
+  # group of exactly two matching digits
+  result = 0
+  (178416..676461).each do |num|
+    if meets_criteria_b(num)
+      result += 1
+    end
+  end
+  print_result(result: result, day: 4, part: "a")
+end
+
 def meets_criteria(number)
   s = number.to_s.split('').map(&:to_i)
   return false if s.size != 6
@@ -33,13 +47,35 @@ def meets_criteria(number)
   has_double
 end
 
+def meets_criteria_b(number)
+  s = number.to_s.split('').map(&:to_i)
+  return false if s.size != 6
+  has_double = false
+  current_run = 1
+  (1..5).each do |pos|
+    return false if s[pos] < s[pos - 1]
+    if s[pos] == s[pos - 1]
+      current_run += 1
+    else
+      has_double = true if current_run == 2
+      current_run = 1
+    end
+  end
+  has_double = true if current_run == 2
+  has_double
+end
+
 def test
   assert(meets_criteria(111111))
   refute(meets_criteria(223450))
   refute(meets_criteria(123789))
   assert(meets_criteria(111123))
   assert(meets_criteria(133567))
+  assert(meets_criteria_b(112233))
+  refute(meets_criteria_b(123444))
+  assert(meets_criteria_b(111122))
 end
 
 test
 part_a
+part_b
