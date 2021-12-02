ebb4364d016c415f413f628210032df9775f53fb
Michael Bryant <mbryantj@gmail.com>
2021-12-01 19:31:21 -0700

    [correct] 2021.1.b in Ruby

--- 2021/day1.rb
+++ 2021/day1.rb
@@ -10,15 +10,30 @@ def count_increases(measurements)
   increases
 end
 
+def trigrams(x)
+  x.zip(x.slice(1..,)).zip(x.slice(2..,)).map(&:flatten).to_a.slice(..-3)
+end
+
+def count_sliding_window_increases(measurements)
+  count_increases(trigrams(measurements).map {|a| a.reduce(:+)})
+end
+
 def part_a
   measurements = File.open('data/day1.txt').readlines.map(&:to_i)
   print_result(result: count_increases(measurements), day: 1, part: 'a')
 end
 
+def part_b
+  measurements = File.open('data/day1.txt').readlines.map(&:to_i)
+  print_result(result: count_sliding_window_increases(measurements), day: 1, part: 'a')
+end
+
 def test
   measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
   assert_equal(7, count_increases(measurements))
+  assert_equal(5, count_sliding_window_increases(measurements))
 end
 
 test
 part_a
+part_b
