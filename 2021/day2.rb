9e505ab6db40b5f0cd228603c2f90da1c36a8e8b
Michael Bryant <mbryantj@gmail.com>
2021-12-04 09:39:50 -0700

    [correct] 2021.2.b in Ruby

--- 2021/day2.rb
+++ 2021/day2.rb
@@ -20,9 +20,39 @@ def compute_a(input)
   x * z
 end
 
+def compute_b(input)
+  # down Y increases aim by Y
+  # up Y decreases aim by Y
+  # forward Y increases x by Y AND increases z by (aim * Y)
+  aim = 0
+  x = 0
+  z = 0
+  input.each do |command|
+    direction, distance = command.split(' ')
+    distance = distance.to_i
+    case direction
+    when 'forward'
+      x += distance
+      z += (aim * distance)
+    when 'down'
+      aim += distance
+    when 'up'
+      aim -= distance
+    else
+      raise StandardError.new("Invalid command: #{command}")
+    end
+  end
+  x * z
+end
+
 def part_a
   input = File.open('data/day2.txt').readlines
   print_result(result: compute_a(input), day: 2, part: 'a')
+end
+
+def part_b
+  input = File.open('data/day2.txt').readlines
+  print_result(result: compute_b(input), day: 2, part: 'b')
 end
 
 def test
@@ -35,7 +65,9 @@ def test
     "forward 2",
   ]
   assert_equal(compute_a(input), 150)
+  assert_equal(compute_b(input), 900)
 end
 
 test
 part_a
+part_b
