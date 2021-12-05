cf55a4766f81a93808bb2ddc23ab344195585bc5
Michael Bryant <mbryantj@gmail.com>
2021-12-05 13:24:27 -0700

    [correct] 2021.3.b in Ruby

--- 2021/day3.rb
+++ 2021/day3.rb
@@ -1,9 +1,6 @@
 require './util'
 
-def compute_a(input)
-  # gamma is the binary number created by finding the most common bit value
-  # in each bit position, in decimal form
-  # epsilon is the same, but with the least common bit
+def get_counts(input)
   string_length = input.first.strip.size
   counts = [0] * string_length
   input.each do |binary_string|
@@ -19,6 +16,14 @@ def compute_a(input)
       counts[index] += binary_string[index].to_i
     end
   end
+  counts
+end
+
+def compute_a(input)
+  # gamma is the binary number created by finding the most common bit value
+  # in each bit position, in decimal form
+  # epsilon is the same, but with the least common bit
+  counts = get_counts(input)
   gamma = counts.map {|count| count > (input.size / 2) ? "1" : "0"}.join.to_i(2)
   epsilon = counts.map {|count| count < (input.size / 2) ? "1" : "0"}.join.to_i(2)
   gamma * epsilon
@@ -30,10 +35,40 @@ def part_a
 end
 
 def compute_b(input)
+  # start with the leftmost bit and discard numbers according to "bit criteria",
+  # then continue with the next leftmost bit until there's only one number left.
+  # oxygen: bit criteria is most common value, 1 breaks ties
+  # co2_scrubber: least common value, 0 breaks ties
+  oxygen = reduce_strings(input, tiebreak: '1', gt: '1', lt: '0')
+  co2_scrubber = reduce_strings(input, tiebreak: '0', gt: '0', lt: '1')
+  oxygen * co2_scrubber
+end
 
+def reduce_strings(input, tiebreak:, gt:, lt:)
+  mod_input = input.dup
+  bit_position = 0
+  while mod_input.size > 1
+    counts = get_counts(mod_input)
+    threshold = (mod_input.size / 2.to_f)
+    selector_value = if counts[bit_position] == threshold
+      tiebreak
+    elsif counts[bit_position] > threshold
+      gt
+    else
+      lt
+    end
+    mod_input = mod_input.filter {|x| x[bit_position] == selector_value}
+    bit_position += 1
+  end
+  if mod_input.size != 1
+    raise StandardError.new("Incorrect reduction: #{mod_input}")
+  end
+  mod_input.first.to_i(2)
 end
 
 def part_b
+  input = File.open("data/day3.txt").readlines
+  print_result(result: compute_b(input), day: 3, part: "b")
 end
 
 def test
@@ -52,6 +87,7 @@ def test
     "01010",
   ]
   assert_equal(compute_a(input), 198)
+  assert_equal(compute_b(input), 230)
 end
 
 test
